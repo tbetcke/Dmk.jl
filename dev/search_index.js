043bd323958832429617789d78975c0907502218
Documenter.jl <documenter@juliadocs.github.io>
2025-03-22 20:38:47 +0000

    build based on a1f34ad

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Dmk","category":"page"},{"location":"#Dmk","page":"Home","title":"Dmk","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Dmk.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Dmk]","category":"page"}]
+}
