bc2870f105c2efbc505ac0197094fbdd689f5d81
Timo Betcke <timo.betcke@gmail.com>
2025-04-01 10:00:42 +0100

    WIP: Adding simd

--- src/chebychev.jl
+++ src/chebychev.jl
@@ -70,15 +70,15 @@ Evaluate aq 2d chebychev interpolant on a 2d tensor grid.
 # Arguments
 - `eval_x::AbstractVector{S}`: The x-coordinates where the interpolant is to be evaluated.
 - `eval_y::AbstractVector{S}`: The y-coordinates where the interpolant is to be evaluated.
-- `values::AbstractMatrix{S}`: The values of the interpolant at the Chebychev points. The first axis is the y-dimension.
-   The second axis is the x dimension.
+- `values::AbstractMatrix{S}`: The values of the interpolant at the Chebychev points. The first axis is the x dimension.
+   The second axis is the y dimension.
 """
-function evaluate2d(eval_x::TX, eval_y::TY, values::V)::Matrix{S} where {S<:Real,TX<:AbstractVector{S},TY<:AbstractVector{S},V<:AbstractMatrix{S}}
+function evaluate2d_tensor(eval_x::TX, eval_y::TY, values::V)::Matrix{S} where {S<:Real,TX<:AbstractVector{S},TY<:AbstractVector{S},V<:AbstractMatrix{S}}
 
-    # The first axis is the y-dimension
-    # So m is the number of Chebychev points in y direction
+    # The first axis is the x-dimension
+    # So m is the number of Chebychev points in x direction
     m = size(values, 1)
-    # n is the number of Chebychev points in the x direction.
+    # n is the number of Chebychev points in the y direction.
     n = size(values, 2)
 
     npx = length(eval_x)
@@ -90,22 +90,172 @@ function evaluate2d(eval_x::TX, eval_y::TY, values::V)::Matrix{S} where {S<:Real
     weights_m = Chebychev.cheb_weights(m)
     weights_n = Chebychev.cheb_weights(n)
 
-    res1 = zeros(S, m, npx)
+    res1 = zeros(S, m, npy)
 
-    # For each y evaluate across the x direction. 
+    # For each x evaluate across the y direction. 
     for j in 1:m
-        res1[j, :] = Chebychev.evaluate1d_impl(eval_x, values[j, :], cheb_points_n, weights_n)
+        res1[j, :] = Chebychev.evaluate1d_impl(eval_y, values[j, :], cheb_points_n, weights_n)
     end
 
-    res2 = zeros(S, npy, npx)
+    res2 = zeros(S, npx, npy)
 
-    # For each x point now evaluate across the y direction.
+    # For each y point now evaluate across the x direction.
 
-    for j in 1:npx
-        res2[:, j] = Chebychev.evaluate1d_impl(eval_y, res1[:, j], cheb_points_m, weights_m)
+    for j in 1:npy
+        res2[:, j] = Chebychev.evaluate1d_impl(eval_x, res1[:, j], cheb_points_m, weights_m)
     end
 
     return res2
 end
+
+"""
+   evaluate2d(eval_x, eval_y, values)
+
+Evaluate a 3d chebychev interpolant on a 3d tensor grid.
+
+# Arguments
+- `eval_x::AbstractVector{S}`: The x-coordinates where the interpolant is to be evaluated.
+- `eval_y::AbstractVector{S}`: The y-coordinates where the interpolant is to be evaluated.
+- `eval_z::AbstractVector{S}`: The z-coordinates where the interpolant is to be evaluated.
+- `values::AbstractArray{S, 3}`: The values of the interpolant at the Chebychev points. The first axis is the x dimension.
+   The second axis is the y dimension. The third is the z dimension.
+"""
+function evaluate3d_tensor(eval_x::TX, eval_y::TY, eval_z::TZ, values::V)::Array{S,3} where {S<:Real,TX<:AbstractVector{S},TY<:AbstractVector{S},TZ<:AbstractVector{S},V<:AbstractArray{S,3}}
+
+    # The first axis is the z-dimension
+    # So m is the number of Chebychev points in x direction
+    m = size(values, 1)
+    # n is the number of Chebychev points in the y direction.
+    n = size(values, 2)
+    # p is the number of Chebychev points in the z direction.
+    p = size(values, 3)
+
+    npx = length(eval_x)
+    npy = length(eval_y)
+    npz = length(eval_z)
+
+
+    cheb_points_m = Chebychev.cheb_points(m)
+    cheb_points_n = Chebychev.cheb_points(n)
+    weights_m = Chebychev.cheb_weights(m)
+    weights_n = Chebychev.cheb_weights(n)
+    cheb_points_p = Chebychev.cheb_points(p)
+    weights_p = Chebychev.cheb_weights(p)
+
+    res1 = zeros(S, m, n, npz)
+
+    # For each x and y evaluate across the z direction. 
+    for i in 1:m
+        for j in 1:n
+            res1[i, j, :] = Chebychev.evaluate1d_impl(eval_z, values[i, j, :], cheb_points_p, weights_p)
+        end
+    end
+
+    res2 = zeros(S, m, npy, npz)
+
+    # Now evaluate across y
+
+    for i in 1:m
+        for j in 1:npz
+            res2[i, :, j] = Chebychev.evaluate1d_impl(eval_y, res1[i, :, j], cheb_points_n, weights_n)
+        end
+    end
+
+    # We have now done the y, z plane for each x point. Now evaluate for each of those points along
+    # the x direction.
+
+    res3 = zeros(S, npx, npy, npz)
+
+    for i in 1:npy
+        for j in 1:npz
+            res3[:, i, j] = Chebychev.evaluate1d_impl(eval_x, res2[:, i, j], cheb_points_m, weights_m)
+        end
+    end
+
+    return res3
+end
+
+
+# function evaluate3d_single_point_impl(x::S, y::S, z::S, values::V, cheb_points::Vector{S}, weights::Vector{S})::S where {S<:Real,V<:AbstractArray{S, 3}}
+
+#     function eval_1d_impl(x::S, values::V, cheb_points::Vector{S}, weights::Vector{S})::S where {S<:Real,V<:AbstractVector{S, 3}}::S
+
+#         n = length(values)
+
+#         numerator::S = 0.0
+#         denominator::S = 0.0
+#         exact_index::Int64 = 0
+
+#         for (index, cheb_point) in enumerate(cheb_points)
+#             diff = x - cheb_point
+#             if iszero(diff)
+#                 exact_index = index
+#             end
+#             diff_inv = 1.0 / diff
+#             numerator += values[index] * weights[index] * diff_inv
+#             denominator += weights[index] * diff_inv
+#         end
+
+#         result = numerator / denominator
+
+#         if exact_index > 0
+#             result = values[exact_index]
+#         end
+
+#         return result
+#     end
+
+#     # We now do the 3d evaluation.
+
+#         # The first axis is the z-dimension
+#     # So m is the number of Chebychev points in z direction
+#     m = size(values, 1)
+#     # n is the number of Chebychev points in the y direction.
+#     n = size(values, 2)
+#     # p is the number of Chebychev points in the x direction.
+#     p = size(values, 3)
+
+
+
+#     cheb_points_m = Chebychev.cheb_points(m)
+#     cheb_points_n = Chebychev.cheb_points(n)
+#     weights_m = Chebychev.cheb_weights(m)
+#     weights_n = Chebychev.cheb_weights(n)
+#     cheb_points_p = Chebychev.cheb_points(p)
+#     weights_p = Chebychev.cheb_weights(p)
+
+#     res1 = zeros(S, m, n, npx)
+
+#     # For each y and z evaluate across the x direction. 
+#     for i in 1:m
+#         for j in 1:n
+#             res1[i, j, :] = Chebychev.evaluate1d_impl(eval_x, values[i, j, :], cheb_points_p, weights_p)
+#         end
+#     end
+
+#     res2 = zeros(S, m, npy, npx)
+
+#     # For each z point now evaluate across all x along the y direction.
+
+#     for i in 1:m
+#         for j in 1:npx
+#             res2[i, :, j] = Chebychev.evaluate1d_impl(eval_y, res1[i, :, j], cheb_points_n, weights_n)
+#         end
+#     end
+
+#     # We have now done the x,y plane for each z point. Now evaluate for each of those points along
+#     # the z direction.
+
+#     res3 = zeros(S, npz, npy, npx)
+
+#     for i in 1:npy
+#         for j in 1:npx
+#             res3[:, i, j] = Chebychev.evaluate1d_impl(eval_z, res2[:, i, j], cheb_points_m, weights_m)
+#         end
+#     end
+
+
+
+# end
 
 end
--- test/chebychev.jl
+++ test/chebychev.jl
@@ -35,31 +35,76 @@ end
     eval_x = range(-1, 1, npx)
     eval_y = range(-1, 1, npy)
 
-    cheb_points_y = Chebychev.cheb_points(m)
-    cheb_weights_y = Chebychev.cheb_weights(m)
-    cheb_points_x = Chebychev.cheb_points(n)
-    cheb_weights_x = Chebychev.cheb_weights(n)
+    cheb_points_y = Chebychev.cheb_points(n)
+    cheb_weights_y = Chebychev.cheb_weights(n)
+    cheb_points_x = Chebychev.cheb_points(m)
+    cheb_weights_x = Chebychev.cheb_weights(m)
 
     values = zeros(m, n)
 
     for i in axes(values, 1)
         for j in axes(values, 2)
-            values[i, j] = exp.(-(3 * cheb_points_x[j]^2 + 5 * cheb_points_y[i]^2))
+            values[i, j] = exp.(-(3 * cheb_points_x[i]^2 + 5 * cheb_points_y[j]^2))
 
         end
     end
 
-    expected_values = zeros(npy, npx)
+    expected_values = zeros(npx, npy)
 
     for i in axes(expected_values, 1)
         for j in axes(expected_values, 2)
-            expected_values[i, j] = exp.(-(3 * eval_x[j]^2 + 5 * eval_y[i]^2))
+            expected_values[i, j] = exp.(-(3 * eval_x[i]^2 + 5 * eval_y[j]^2))
 
         end
     end
 
-    actual_values = Chebychev.evaluate2d(eval_x, eval_y, values)
+    actual_values = Chebychev.evaluate2d_tensor(eval_x, eval_y, values)
+
+    println("max error: ", maximum(abs.((expected_values - actual_values) ./ (expected_values))))
 
     @test maximum(abs.((expected_values - actual_values) ./ (expected_values))) < 1E-14
 
+end
+
+@testitem "test chebychev polynomial 3d" begin
+    import Dmk.Chebychev
+    using LinearAlgebra
+
+    m = 70
+    n = 80
+    p = 90
+
+    npx = 100
+    npy = 200
+    npz = 80
+
+    eval_x = range(-1, 1, npx)
+    eval_y = range(-1, 1, npy)
+    eval_z = range(-1, 1, npz)
+
+    cheb_points_y = Chebychev.cheb_points(n)
+    cheb_weights_y = Chebychev.cheb_weights(n)
+    cheb_points_x = Chebychev.cheb_points(m)
+    cheb_weights_x = Chebychev.cheb_weights(m)
+    cheb_points_z = Chebychev.cheb_points(p)
+    cheb_weights_z = Chebychev.cheb_weights(p)
+    values = zeros(m, n, p)
+    for i in axes(values, 1)
+        for j in axes(values, 2)
+            for k in axes(values, 3)
+                values[i, j, k] = exp.(-(3 * cheb_points_x[i]^2 + 5 * cheb_points_y[j]^2 + 6 * cheb_points_z[k]^2))
+            end
+        end
+    end
+
+    expected_values = zeros(npx, npy, npz)
+    for i in axes(expected_values, 1)
+        for j in axes(expected_values, 2)
+            for k in axes(expected_values, 3)
+                expected_values[i, j, k] = exp.(-(3 * eval_x[i]^2 + 5 * eval_y[j]^2 + 6 * eval_z[k]^2))
+            end
+        end
+    end
+    actual_values = Chebychev.evaluate3d_tensor(eval_x, eval_y, eval_z, values)
+    @test maximum(abs.((expected_values - actual_values) ./ (expected_values))) < 1E-14
 end
